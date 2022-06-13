f34b6a37f1b55ac9729349a9d8726c68a23001c5
Kat <katziv@students.cs.ubc.ca>
2022-06-14 01:35:37 +0200

    forgot to commit these

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,16 +4,27 @@ import "./Card.css";
 interface CardProps {
   title: string,
   desc: string,
+  backgroundColor?: string,
 }
 
-const Card = ({ title, desc }: CardProps) => {
+const defaultProps: CardProps = {
+  title: '',
+  desc: '',
+  backgroundColor: '#ffffff'
+}
+
+const Card = ({ title, desc, backgroundColor}: CardProps) => {
   return (
     <div className="card">
-      <SettingsOutlinedIcon fontSize="large" color="primary" id="icon" />
+      <div className="circle" style={{backgroundColor: backgroundColor}}>
+        <SettingsOutlinedIcon fontSize="large" color='primary' id="icon" />
+      </div>
       <h3 id="card-title">{title}</h3>
       <p id="card-desc">{desc}</p>
     </div>
   );
 };
+
+Card.defaultProps = defaultProps;
 
 export default Card;
