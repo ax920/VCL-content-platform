7188cf88b4f8744656c202ecd3e97b46c8967d48
Sara Ibrahim <71746168+svolterra@users.noreply.github.com>
2022-09-30 19:00:40 -0700

    Sarah/ Add Mission Statement to Landing Page. (#26)
    * Re-implement mission statement due to branching issues.
    
    * Make mission statement more responsive to smaller screens.
    
    * Address code review comments and reformat layout.
    
    * Align mission statement text width with Figma design.
    
    * Add mission statement footer.
    
    * Reformat footer for smaller screens.
    
    * Address CR comments and adjust UI.
    
    Co-authored-by: ElisaHu1 <104804980+ElisaHu1@users.noreply.github.com>

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "@components/Card";
 import "./Home.css"
+import { TEXT } from '@statics';
 
 interface HomeProps {}
 
@@ -13,6 +14,24 @@ const Home: React.FC<HomeProps> = (props) => {
         <Card title="Card Title" desc="Description of how the lab can help COGS students with this project" />
         <Card title="Card Title" desc="Description of how the lab can help COGS students with this project" />
         <Card title="Card Title" desc="Description of how the lab can help COGS students with this project" />
+      </div>
+      <div className="mission-statement-container">
+        <div className="mission-statement-title">
+            <p>{TEXT.LANDING_PAGE.MISSION_STATEMENT.TITLE}</p>
+            <hr className="title-underline"/>
+        </div>
+        <div>
+          <p className="mission-statement">{TEXT.LANDING_PAGE.MISSION_STATEMENT.LAB_GOALS}</p>
+          <p className="mission-statement">{TEXT.LANDING_PAGE.MISSION_STATEMENT.LAB_GOALS}</p>
+        </div>
+      </div>
+      <div className="mission-img-container">
+        <div className="mission-img-placeholder-one"></div>
+        <div className="mission-img-placeholder-two"></div>
+      </div>
+      <div className="mission-statement-footer">
+        <hr className="footer-divider"/>
+        <p>{TEXT.LANDING_PAGE.MISSION_STATEMENT.LAB_GOALS}</p>
       </div>
     </div>
   );
