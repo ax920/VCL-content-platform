6481dd6ad9287a9ee62f3e10742d9f2cd7f6f9ea
katarina <kat.zivkovic00@gmail.com>
2022-10-26 09:58:28 -0700

    landing page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,8 +9,7 @@ interface HomeProps {}
 const Home: React.FC<HomeProps> = (props) => {
   return (
     <div className="Home">
-      <LandingPage></LandingPage>
-      <div>HI THIS IS THE LANDING PAGE</div>
+      <LandingPage/>
       <div className="card-container">
         <Card title="Card Title" desc="Description of how the lab can help COGS students with this project" />
         <Card title="Card Title" desc="Description of how the lab can help COGS students with this project" />
