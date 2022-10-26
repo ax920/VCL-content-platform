7ec2e5a719c7958b49f8bfd4e0b11bb2ccb321e6
krarpit <kumar.arpit77@yahoo.com>
2022-10-26 13:51:04 -0700

    new components, color constants

---
+++ src/components/VerticalSpacer/VerticalSpacer.tsx
@@ -0,0 +1,7 @@
+const VerticalSpacer = ({height}: {height: number}) => {
+    return (
+        <div style={{ "height": `${height}px` }} />
+    )
+}
+
+export default VerticalSpacer;
--- src/services/adapters/projectAdapter.ts
+++ src/services/adapters/projectAdapter.ts
@@ -2,14 +2,20 @@ import type { BaseResponse } from './types'
 import projectAPI from '../api/projectAPI'
 import { Project } from '@entities/Project'
 
-interface ProjectResponse extends BaseResponse {
+// array of projects
+export interface ProjectsResponse extends BaseResponse {
     data? : Project[]
 } 
 
-export const callGetAllProjectsAPI = async (): Promise<ProjectResponse> => {
+// single project
+export interface ProjectResponse extends BaseResponse {
+    data?: Project
+}
+
+export const callGetAllProjectsAPI = async (): Promise<ProjectsResponse> => {
     try {
         const res = await projectAPI.getAllProjects()
-        return res.data as ProjectResponse;
+        return res.data as ProjectsResponse;
     } catch (err: any) {
         return err.response.data as BaseResponse;
     }
